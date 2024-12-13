dfe97db31d51f850de1b3ac0695f71c4eaff36a1
Joás Martins <joasmcarmo@gmail.com>
2024-12-13 09:55:24 -0300

    Webhook 2

--- src/useCases/WebhookPIX.ts
+++ src/useCases/WebhookPIX.ts
@@ -1,5 +1,6 @@
-import payments from "../payment"
+//import payments from "../payment"
 
 export default async function WebhookPIX(req, res) {
+    console.log("WEBHOOK CHAMADO")
     return res.status(200).json("Hello World!")
 }
