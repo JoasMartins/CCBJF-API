18e53e76f2fcb0e33a13861f19327e3d409680e1
Joás Martins <joasmcarmo@gmail.com>
2024-12-13 09:37:23 -0300

    Update nas credenciais de certificado 2

--- credentials.ts
+++ credentials.ts
@@ -4,6 +4,6 @@ export default {
     sandbox: process.env.PIX_SANDBOX === 'true' ? true : false,
     client_id: process.env.PIX_CLIENT_ID,
     client_secret: process.env.PIX_CLIENT_SECRET,
-    pix_cert: `${__dirname}/certificates/${process.env.PIX_CERTIFICATE}`,
     certificate: `${__dirname}/certificates/${process.env.PIX_CERTIFICATE}`,
+    pix_cert: `${__dirname}/certificates/${process.env.PIX_CERTIFICATE}`,
 }
