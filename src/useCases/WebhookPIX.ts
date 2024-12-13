c8abb5c5c3a3b6a758e2b203b6e9fb68082b7c59
Joás Martins <joasmcarmo@gmail.com>
2024-12-13 10:54:18 -0300

    Webhook 6

--- src/useCases/WebhookPIX.ts
+++ src/useCases/WebhookPIX.ts
@@ -1,10 +1,12 @@
 
 
 export default async function WebhookPIX(req, res) {
-    console.log(`Token de acesso tentado: ${req.headers.token_acess} |=| ${process.env.TOKEN_ACESS}`)
+    console.log(req.headers)
+    //console.log(`Token de acesso tentado: ${req.headers.token_acess} |=| ${process.env.TOKEN_ACESS}`)
     if(req.headers.token_acess != process.env.TOKEN_ACESS) {
         console.error("Token de acesso inválido!")
-        return res.status(401).json("INVALID_TOKEN")
+        res.status(401).json("INVALID_TOKEN")
+        return 
     }
 
     return res.status(200).json("Hello World!")
