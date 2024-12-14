04c01d598e390ffeefdc708d4b59734fdad647d1
Joás Martins <joasmcarmo@gmail.com>
2024-12-14 13:54:02 -0300

    Novo sistema de variaveis de ambiente

--- credentials.ts
+++ credentials.ts
@@ -1,9 +1,9 @@
-require('dotenv').config({ path: process.env.NODE_ENV === 'prod' ? '.env' : '.env.local' });
+require('dotenv').config({ path: '.env'});
 
 export default {
-    sandbox: process.env.PIX_SANDBOX === 'true' ? true : false,
-    client_id: process.env.PIX_CLIENT_ID,
-    client_secret: process.env.PIX_CLIENT_SECRET,
-    certificate: `${__dirname}/certificates/${process.env.PIX_CERTIFICATE}`,
+    sandbox: process.env.NODE_ENV === 'prod'?  false : true,
+    client_id: process.env.NODE_ENV === 'prod' ? process.env.PROD_PIX_CLIENT_ID : process.env.HOMO_PIX_CLIENT_ID,
+    client_secret: process.env.NODE_ENV === 'prod' ? process.env.PROD_PIX_CLIENT_SECRET : process.env.HOMO_PIX_CLIENT_SECRET,
+    certificate: process.env.NODE_ENV === 'prod' ? `${__dirname}/certificates/${process.env.PROD_PIX_CERTIFICATE}` : `${__dirname}/certificates/${process.env.HOMO_PIX_CERTIFICATE}`,
     //pix_cert: `${__dirname}/certificates/${process.env.PIX_CERTIFICATE}`,
 }
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,13 @@ import options from "../credentials"
 
 console.log(options)
 
-require('dotenv').config({ path: process.env.NODE_ENV === 'prod' ? '.env' : '.env.local' });
+//require('dotenv').config({ path: process.env.NODE_ENV === 'prod' ? '.env' : '.env.local' });
 
+function ConfigEnv() {
+    if(process.env.NODE_ENV === 'prod') {
+        
+    }
+}
 
 app.listen(4000, async () => {
     console.log("Servidor ligado! Ambiente: " + process.env.NODE_ENV)
