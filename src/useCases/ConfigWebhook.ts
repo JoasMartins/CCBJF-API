047ddebe95ba797582450a9a4d5631edb4487867
Joás Martins <joasmcarmo@gmail.com>
2024-12-14 13:58:27 -0300

    Webhook config

--- src/useCases/ConfigWebhook.ts
+++ src/useCases/ConfigWebhook.ts
@@ -2,17 +2,24 @@ import EfiPay from 'sdk-typescript-apis-efi';
 import options from '../../credentials';
 
 export default async function ConfigWebhook(req, res) {
-    options['validateMtls'] = false
+    try {
+        options['validateMtls'] = false
 
-    let body = {
-        webhookUrl: 'https://api.applicco.com.br/prod/webhook',
+        let body = {
+            webhookUrl: 'https://api.applicco.com.br/prod/webhook',
+        }
+    
+        let params = {
+            chave: 'contato@applicco.com.br',
+        }
+    
+        const efipay = new EfiPay(options)
+
+        let result = await efipay.pixConfigWebhook(params, body)
+    
+        return res.status(200).json(result)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json(error)
     }
-
-    let params = {
-        chave: 'contato@applicco.com.br',
-    }
-
-    const efipay = new EfiPay(options)
-
-    return await efipay.pixConfigWebhook(params, body)
 }
